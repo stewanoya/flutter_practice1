3d464ba121d9e39117614ab9aeb8d011ec07a08f
stewanoya <stewartanoya@gmail.com>
2022-03-02 15:01:11 -0500

    Create getter to display result phrase

--- lib/result.dart
+++ lib/result.dart
@@ -5,10 +5,22 @@ class Result extends StatelessWidget {
 
   Result(this.totalScore);
 
+  String get resultPhrase {
+    // can add if statements in here to change the statement based on result score.
+    return "You did it, your final score was: $totalScore";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You did it, your final score was: $totalScore"),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
