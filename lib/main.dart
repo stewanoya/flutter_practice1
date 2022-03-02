db49f60e55d9acc3b854744849cbe6be94c86266
stewanoya <stewartanoya@gmail.com>
2022-03-02 09:06:37 -0500

    add app bar and body

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override // common practice - if providing method that also exists in the class 
+  @override // common practice - if providing method that also exists in the class
   // that I'm extending
-  // let dart know that the override is deliberate. 
+  // let dart know that the override is deliberate.
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello, wow it's working!!"),
+      // scaffold creates a basic page design (color, layout etc.)
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Text("This is default text"),
+      ),
     );
   }
 }
